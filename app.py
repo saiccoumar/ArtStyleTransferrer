7cd1002fac1931d74956658b2042e8d8f2ed48d0
Sai Coumar <55699636+saiccoumar@users.noreply.github.com>
2021-05-06 17:56:32 -0500

    semi working

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
 
 from flask import Response
-from flask import Flask
+from flask import Flask, request, flash, redirect, url_for
 from flask import render_template
 import tensorflow as tf
 import os
 import tensorflow_hub as hub
 import matplotlib.pyplot as plt
-import time
-from PIL import Image
-import numpy as np
-os.environ["TFHUB_DOWNLOAD_PROGRESS"] = "True"
+import random
+from werkzeug.utils import secure_filename
+import matplotlib
+matplotlib.use('Agg')
 
 current_directory = os.getcwd()
 print(current_directory)
 statDir = current_directory+'/static/'
 templateDir = current_directory+'/templates/'
+artistList=['kandinskiy','monet','nagai','banksy','dali','kahlo','gogh','picasso']
+
+
+
+
 
 # initialize a flask object
 app = Flask(__name__,static_folder=statDir,
             template_folder=templateDir)
 
-# add list and iteration through style path
-content_path = current_directory+"/selfie.JPG"
-style_path = current_directory+"/kandinskiy3.jpg"
+# add list and iteration here
+content_path = current_directory+"/IMG_7080.jpg"
+
+
 
 # 256 to 512 dimensions appear to be the most effective at creating art
-def scale(image, max_dim = 256):
+def scale(image, max_dim = 512):
   # Casts a tensor to a new type.
   original_shape = tf.cast(tf.shape(image)[:-1], tf.float32)
 
@@ -58,51 +64,6 @@ def load_img(path_to_img):
   # the model requires a 4-dimensional Tensor
   return img[tf.newaxis, :]
 
-def preprocess_image(image_path):
-  """ Loads image from path and preprocesses to make it model ready
-      Args:
-        image_path: Path to the image file
-  """
-  hr_image = tf.image.decode_image(tf.io.read_file(image_path))
-  # If PNG, remove the alpha channel. The model only supports
-  # images with 3 color channels.
-  if hr_image.shape[-1] == 4:
-    hr_image = hr_image[...,:-1]
-  hr_size = (tf.convert_to_tensor(hr_image.shape[:-1]) // 4) * 4
-  hr_image = tf.image.crop_to_bounding_box(hr_image, 0, 0, hr_size[0], hr_size[1])
-  hr_image = tf.cast(hr_image, tf.float32)
-  return tf.expand_dims(hr_image, 0)
-
-def save_image(image, filename):
-  """
-    Saves unscaled Tensor Images.
-    Args:
-      image: 3D image tensor. [height, width, channels]
-      filename: Name of the file to save to.
-  """
-  if not isinstance(image, Image.Image):
-    image = tf.clip_by_value(image, 0, 255)
-    image = Image.fromarray(tf.cast(image, tf.uint8).numpy())
-  image.save("%s.jpg" % filename)
-  print("Saved as %s.jpg" % filename)
-
-content_image = load_img(content_path)
-style_image = load_img(style_path)
-
-# %matplotlib inline
-def plot_image(image, title=""):
-  """
-    Plots images from image tensors.
-    Args:
-      image: 3D image tensor. [height, width, channels].
-      title: Title to display in the plot.
-  """
-  image = np.asarray(image)
-  image = tf.clip_by_value(image, 0, 255)
-  image = Image.fromarray(tf.cast(image, tf.uint8).numpy())
-  plt.imshow(image)
-  plt.axis("off")
-  plt.title(title)
 
 # plt.figure(figsize=(12, 12))
 # plt.subplot(1, 2, 1)
@@ -113,50 +74,78 @@ def plot_image(image, title=""):
 # plt.title('Style Image')
 
 # plt.show()
-
+def convertAll(image):
+  for i in artistList:
+    convert(image,i)
 
 
 # Load Magenta's Arbitrary Image Stylization network from TensorFlow Hub  
-hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/1')
+# premade models sometimes get updated and need to be replaced.
+hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
 
 print("checkpoint 1")
+def convert(image,artist):
+  kandinskiy_style = current_directory+"/art"+"/"+artist+str(random.randint(1,3) )+".jpg"
+  content_image = load_img(image)
+  style_image = load_img(kandinskiy_style)
 # Pass content and style images as arguments in TensorFlow Constant object format
-stylized_image = hub_module(tf.constant(content_image), tf.constant(style_image))[0]
-print("checkpoint 2")
+  stylized_image = hub_module(tf.constant(content_image), tf.constant(style_image))[0]
+  print("checkpoint 2:"+kandinskiy_style)
 
-
-IMAGE_PATH = stylized_image
-SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
-
-hr_image = preprocess_image(IMAGE_PATH)
-plot_image(tf.squeeze(hr_image), title="Original Image")
-model = hub.load(SAVED_MODEL_PATH)
-start = time.time()
-fake_image = model(hr_image)
-fake_image = tf.squeeze(fake_image)
-print("Time Taken: %f" % (time.time() - start))
-plot_image(tf.squeeze(fake_image), title="Super Resolution")
-# save_image(tf.squeeze(hr_image), filename="Original Image")
 # Set the size of the plot figure
-# plt.figure(figsize=(12, 12))
+  plt.figure(figsize=(6, 6))
 
-# # Plot the stylized image
-# plt.imshow(stylized_image[0])
+# Plot the stylized image
+  plt.imshow(stylized_image[0])
 
-# # Add title to the plot
-# plt.title('Stylized Image')
+  # Add title to the plot
+  # plt.title('Stylized Image')
 
-# # Hide axes
-# plt.axis('off')
+# Hide axes
+  plt.axis('off')
 
-# # Show the plot
-# plt.show()
+# Show the plot
+  plt.savefig(current_directory+"/static/output/"+artist+"Output.jpg")
+# convertAll(content_path)
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
-@app.route("/")
-def index():
-	# return the rendered template
-	return render_template("index.html")
+  
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+  if request.method == 'POST':
+        # check if the post request has the file part
+      if 'file' not in request.files:
+          flash('No file part')
+          return redirect(request.url)
+      file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+      if file.filename == '':
+          flash('No selected file')
+          return redirect(request.url)
+      if file and file.filename:
+        filename = secure_filename(file.filename)
+        file.save(current_directory+'/uploads/'+filename)
+        for r in artistList:
+          convert(("uploads/"+filename),r)
+          print(r+" converted")
+        return render_template("images.html")
+  return render_template("index.html")
+
+convert("selfie.jpg","banksy")
+  
 
 # check to see if this is the main thread of execution
 if __name__ == '__main__':
-	app.run(port=8000, debug=True, use_reloader=False)
+  app.run(port=8000, debug=True, use_reloader=True)
